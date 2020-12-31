ca0de52d94c531f6fbe93bca6bf70b03b95ccda1
kkarung <52196875+kkarung@users.noreply.github.com>
2020-12-31 11:19:07 +0900

    Create 5622.c

---
+++ prob_1000/5622.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	int abc[26] = { 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10 };
+	int c, s = 0;
+
+	while ((c = getchar()) != '\n')
+		s += abc[c - 65];
+
+	printf("%d", s);
+}
