2c28186b8d4f77033565d873b0307ad7b4b73aaf
kkarung <52196875+kkarung@users.noreply.github.com>
2021-01-20 20:11:58 +0900

    Create 11720.cpp

---
+++ prob_10000/11720.cpp
@@ -0,0 +1,14 @@
+// char을 입력 받으면 한 글자씩 입력 받을 수 있음을 깜빡했음
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum=0;
+    char c;
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        cin >> c;
+        sum += c-'0';
+    }
+    cout << sum;
+}
