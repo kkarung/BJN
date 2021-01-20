0786aa404d104d568dc2be3a0a96f110b567e755
kkarung <52196875+kkarung@users.noreply.github.com>
2021-01-20 22:04:16 +0900

    Create 2440.cpp

---
+++ prob_1000/2440.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while (n-->0) {
+        for (int i=0; i<=n; i++) cout << "*";
+        cout << "\n";
+    }
+}
