fa7edf44efc51ed7d42dd6e196a175e3b97af986
kkarung <52196875+kkarung@users.noreply.github.com>
2021-01-20 23:15:34 +0900

    Create 10991.cpp

---
+++ prob_10000/10991.cpp
@@ -0,0 +1,13 @@
+// 2438, 2439, 2440, 2441, 2442, 2445, 2522, 2446, 10991, 10992 별 찍기 문제
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i=1; i<=n; i++) {
+        for (int j=0; j<n-i; j++) cout << " ";
+        for (int j=0; j<i; j++) cout << "* ";
+        cout << "\n";
+    }
+}
