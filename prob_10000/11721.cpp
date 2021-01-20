bc118a1ee7428343a1a7278b495415afabe7b092
kkarung <52196875+kkarung@users.noreply.github.com>
2021-01-20 20:50:54 +0900

    Update 11721.cpp

--- prob_10000/11721.cpp
+++ prob_10000/11721.cpp
@@ -1,5 +1,6 @@
 // substr 쓰는 방법도 있고 scanf 쓰는 방법도 있음
 // 개인적으로는 scanf 쓰는 방법이 간단해 보임
+// https://www.acmicpc.net/source/24809285(하나의 string으로 만들어서 출력하는 방법)도 있음
 #include <iostream>
 using namespace std;
 
