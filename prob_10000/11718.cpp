2919dd22b25ba9a20357aa76fed9c293cdc39c74
kkarung <52196875+kkarung@users.noreply.github.com>
2021-01-20 17:08:11 +0900

    Create 11718.cpp

---
+++ prob_10000/11718.cpp
@@ -0,0 +1,13 @@
+// getline (string) 사용함
+// https://noosphere.tistory.com/5 여기 참고하면 좋을 거 같음 getline를 잘 쓰셨다.
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+     do {
+        getline(cin, s);
+        cout << s << endl;
+    } while (s!="\0") ;
+}
