72416cb26fb408d33f407c673a59aa7f6ab3ada5
kkarung <52196875+kkarung@users.noreply.github.com>
2021-01-29 00:01:02 +0900

    Update 1463.cpp (DP/Bottom-up)

--- prob_1000/1463.cpp
+++ prob_1000/1463.cpp
@@ -1,3 +1,8 @@
+/////////////////////////
+// 2021.01.28 / kkarung /
+//   DP / Bottom-up
+/////////////////////////
+
 #include <iostream>
 using namespace std;
 int main() {
@@ -12,4 +17,40 @@ int main() {
         list[i] = (list[i] > list[i-1]+1 ? list[i-1]+1 : list[i]);
     }
     cout << list[n];
+}
+
+////// 개션1: list[i] 초기화 부분을 빼자 //////
+    
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    int list[1000001];
+    list[1]=0;
+    cin >> n;
+    for (int i=4; i<=n; i++) {
+        list[i] = list[i-1]+ 1;
+        if (!(i%3)) list[i] = list[i/3]+1;
+        if (!(i%2)) list[i] = (list[i] > list[i/2]+1 ? list[i/2]+1 : list[i]);
+    }
+    cout << list[n];
+}
+
+///// 개선2: Top-down 방식을 쓰자! //////
+// 출처: https://www.acmicpc.net/source/6296573
+
+#include<cstdio>
+#include<algorithm>
+int f(int n) {
+	int a,b;
+	if(n<2) 
+		return 0;
+	a=f(n/2)+n%2+1;
+	b=f(n/3)+n%3+1;
+	return std::min(a,b);
+}
+main(){
+	int n;
+	scanf("%d",&n);
+	printf("%d",f(n));
 }
