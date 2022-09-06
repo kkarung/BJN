1e4f7435e625b9e3013a48e33c94d26284c50d65
kkarung <52196875+kkarung@users.noreply.github.com>
2022-09-06 17:51:10 +0900

    Update 2104.cpp

--- prob_1000/2104.cpp
+++ prob_1000/2104.cpp
@@ -5,15 +5,15 @@
 ////////////////////////////// report ////////////////////////////////////
 // 부분 배열이 아니라 전체 배열에서 최댓값이 있다 가정하고, 이 값을 구하는 방법을 생각해보자
 // 무조건 가운데에 위치한 element는 포함된다. 이를 왼쪽으로, 또는 오른쪽으로 greedy하게 끝까지 이동하면
-// 전체 배열에서의 최댓값을 찾을 수 있다. (calArray 함수)
+// 전체 배열에서의 최댓값을 찾을 수 있다.
 // 이제 Divide and Conquer 방식을 이용하여 왼쪽 절반에서 최댓값을 찾고, 오른쪽 절반에서도 똑같이 (이하 생략)
-// (maxArray 함수)
 
 #include <iostream>
 using namespace std;
 
 int a[100000] = {0};
 
+// 현재 배열에서 최댓값을 찾는 함수
 long long int calArray (int i, int j) {
     int mid = (i+j)/2;
     int left = mid-1, right = mid+1; // greedy하게 이동할 후보
