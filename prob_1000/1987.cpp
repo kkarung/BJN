24ab5164b0ee422d86483f192b88baaa3a0368dd
kkarung <52196875+kkarung@users.noreply.github.com>
2022-10-05 23:57:38 +0900

    Create 1987.cpp

---
+++ prob_1000/1987.cpp
@@ -0,0 +1,42 @@
+//////////////////////////
+// 2022.10.05 / kkarung //
+//       백트래킹         //
+//////////////////////////
+///////////////////////// report ///////////////////////////////
+// 기본적인 백트래킹 문제
+
+#include <iostream>
+using namespace std;
+#define OOB 21
+
+const int dr[4] = {0, 0, 1, -1};
+const int dc[4] = {1, -1, 0, 0};
+
+int R, C, M = 0;
+string board[OOB] = {};
+bool visited[OOB][OOB] = {false};
+bool isUsed[26] = {false};
+
+void backTracking(int r, int c, int cnt) {
+	M = max(M, cnt+1); // 이제까지의 모든 
+	visited[r][c]=true;
+	visited[r][c]=true;
+	isUsed[board[r][c]-'A']=true;
+	for (int i=0; i<4; i++) {
+		int nr = r+dr[i];
+		int nc = c+dc[i];
+		if (nr<0||nr>=R||nc<0||nc>=C) continue;
+		if (!isUsed[board[nr][nc]-'A'])
+			backTracking(nr, nc, cnt+1);
+	}
+	isUsed[board[r][c]-'A']=false;
+}
+
+int main() {
+	cin >> R >> C;
+	for (int i=0; i<R; i++)
+		cin >> board[i];
+	
+	backTracking(0, 0, 0);
+	cout << M;
+}
