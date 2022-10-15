98363d01614321bef09ab245cf43097480d29aef
kkarung <52196875+kkarung@users.noreply.github.com>
2022-10-15 23:07:32 +0900

    Create 11279.cpp

---
+++ prob_10000/11279.cpp
@@ -0,0 +1,31 @@
+//////////////////////////
+// 2022.10.15 / kkarung //
+//    priority queue    //
+//////////////////////////
+///////////////////////// report ///////////////////////////////
+// 시간 제한이 1초이므로 시간복잡도에 유의해야 한다.
+// priority queue를 사용하면 삽입, 삭제 모두 O(logn)이고 n번 반복하므로 O(nlogn)이다
+// 최대 N은 10의 6승이므로 시간 조건을 만족한다
+
+#include <iostream>
+#include <queue>
+using namespace std;
+#define MAXN 100001
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int N, x;
+	priority_queue<int> pq;
+	cin >> N;
+
+	while(N--) {
+		cin >> x;
+		if (x) pq.push(x);
+		else if (pq.size() == 0) cout << "0\n";
+		else {
+			cout << pq.top() << '\n';
+			pq.pop();
+		}
+	}
+}
