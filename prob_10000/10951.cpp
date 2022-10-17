373e58930ce0476abcdabd146db6dbdc7a1aaf83
kkarung <52196875+kkarung@users.noreply.github.com>
2022-10-17 13:04:49 +0900

    Update 10951.cpp

--- prob_10000/10951.cpp
+++ prob_10000/10951.cpp
@@ -1,7 +1,14 @@
+//////////////////////////
+// 2022.10.17 / kkarung //
+//        입출력          //
+//////////////////////////
+////////////////////////// report ////////////////////////////////
+// https://skku.goorm.io/qna/4241을 보면 cin의 return형이 연산 성공/실패에 대한 bool 값임을 알 수 있다
+
 #include <iostream>
 using namespace std;
 
 int main() {
     int a,b;
-    while (cin >> a >> b) cout << a+b << endl;
+    while (cin >> a >> b) cout << a+b << '\n';
 }
