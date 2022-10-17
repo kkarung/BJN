af6a957065abd64dbde5de8a71a6623c56e3b347
kkarung <52196875+kkarung@users.noreply.github.com>
2022-10-17 13:09:29 +0900

    Update 10951.cpp

--- prob_10000/10951.cpp
+++ prob_10000/10951.cpp
@@ -3,7 +3,8 @@
 //        입출력          //
 //////////////////////////
 ////////////////////////// report ////////////////////////////////
-// https://skku.goorm.io/qna/4241을 보면 cin의 return형이 연산 성공/실패에 대한 bool 값임을 알 수 있다
+// https://skku.goorm.io/qna/4241을 보면 cin은 cin 객체를 반환하지만
+// if나 while안에 들어갈 시 operator에 의해 연산 성공/실패에 대한 bool값을 반환함을 알 수 있다
 
 #include <iostream>
 using namespace std;
