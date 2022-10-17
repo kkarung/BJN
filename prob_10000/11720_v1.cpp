29176956eabb3e0d716716f5597bc3d3842f58e7
kkarung <52196875+kkarung@users.noreply.github.com>
2022-10-17 14:13:01 +0900

    Update and rename 11720.cpp to 11720_v1.cpp

--- prob_10000/11720_v1.cpp
+++ prob_10000/11720_v1.cpp
@@ -1,5 +1,6 @@
 // char을 입력 받으면 한 글자씩 입력 받을 수 있음을 깜빡했음
 // https://blockdmask.tistory.com/78 참고함
+
 # include <iostream>
 using namespace std;
 
@@ -10,23 +11,6 @@ int main() {
     for (int i=0; i<n; i++) {
         cin >> c;
         sum += c-'0';
-    }
-    cout << sum;
-}
-
----------------------------------------------
-// 그냥 한꺼번에 받기
-
-#include <iostream>
-using namespace std;
-
-int main() {
-    int n, sum=0;
-    char num[100];
-    cin >> n;
-    cin >> num;
-    while (n--) {
-        sum += num[n]-'0';
     }
     cout << sum;
 }
